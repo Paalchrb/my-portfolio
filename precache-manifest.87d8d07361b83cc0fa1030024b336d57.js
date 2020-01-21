7436b35fea0104712075d66515973dcbe99aefd8
Pål-Christian By <Pal.christian.by@gmail.com>
2020-01-21 12:03:29 +0100

    Updates

---
+++ precache-manifest.87d8d07361b83cc0fa1030024b336d57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3515cf64342ea5d296f9bbdbf31257bc",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "bc13bfd20b8ed96cb2bd",
+    "url": "/my-portfolio/static/css/main.6c633206.chunk.css"
+  },
+  {
+    "revision": "0eed85bbfd1aec3bdd6c",
+    "url": "/my-portfolio/static/js/2.6bd59a3b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my-portfolio/static/js/2.6bd59a3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc13bfd20b8ed96cb2bd",
+    "url": "/my-portfolio/static/js/main.a467c4d0.chunk.js"
+  },
+  {
+    "revision": "b66a9665c859b16ac6cf",
+    "url": "/my-portfolio/static/js/runtime-main.7646dcd2.js"
+  },
+  {
+    "revision": "da5f00f7002b36f8da7ebcbb5627ee33",
+    "url": "/my-portfolio/static/media/portfolio.da5f00f7.png"
+  }
+]);
